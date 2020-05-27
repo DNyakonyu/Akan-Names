1eb25432b67316258e21cf0d91fb664addbcf5ca
DUNCAN NYAKONYU <Dndirangu@safaricom.co.ke>
2020-05-27 14:21:10 -0700

    modified README.md

---
+++ README.md
@@ -0,0 +1,49 @@
+# Akan Names
+
+
+## Author
+
+DNyakonyu
+
+## Description
+
+This project is a web application that takes a user's birthday and calculates the day of the week they were born and then depending on their gender displays their Akan-Name.
+
+
+## IP URL
+
+https://DNyakonyu.github.io/Akan-Names
+
+## Technologies used
+
+### HTML()
+### CSS()
+### JS()
+### Bootstrap() &
+### markdown()
+
+## BDD
+
+Enter century of birth eg 20 for 2020, Input Example 20 Output example 20
+Enter year of Birth eg 14 for 2014 Input Example 14 Output Example 14
+
+Enter month of birth eg 4 for April Input example 4 Output example 4
+
+Enter Date of the month you were born in eg 27 input example 27 output example 207
+
+Select gender Input example male output male
+
+Select gender Input example female output female
+
+If the above is submitted. Input example, the program compares the inputs with the conditions set for the inputs eg if No input field is empty, the program executes else an alert to enter required infomation.
+
+#### Example
+
+century input - 20
+year input - 20
+month input - 5
+date input - 27
+gender - female selected, Expected output is You were born on Wednesday and Your akan name is Akua!
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var CC, YY, MM, DD, d, dayValue;
+var CC, YY, MM, DD, d, birthDay;
 var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
@@ -32,13 +32,13 @@ function validate() {
 
 }
 
-function calculateDayValue(){
+function calculatebirthDay(){
   year = document.getElementById("year").value;
   CC = parseInt(year.substring(0,2));
   YY = parseInt(year.substring(2,4));
   MM = parseInt(document.getElementById("month").value);
   DD = parseInt(document.getElementById("date").value);
-  d = ( ( (CC/4) -2*CC-1) + ( (5*YY/4) ) + ((26*(MM+1)/10) ) + DD)%7;
+  d = ( ( (CC/4) -2*CC-1) + ( (5*YY/4) ) + ((26*(MM+1)/10) ) + DD+1)%7;
   console.log(d);
   return (Math.floor(d));
 }
@@ -56,47 +56,47 @@ function getGender(){
   }
   switch(gender){
     case "male":
-      if (dayValue == 1){
+      if (birthDay == 1){
         alert("You were born on " +dayNames[0] + " and Your akan name is " +maleNames[0]+"!");
       }
-      else if(dayValue == 2){
+      else if(birthDay == 2){
         alert("You were born on "+dayNames[1] + " and Your akan name is " +maleNames[1]+"!");
       }
-      else if(dayValue == 3){
+      else if(birthDay == 3){
         alert("You were born on " +dayNames[2]+ " and Your akan name is " +maleNames[2]+"!");
       }
-      else if(dayValue == 4){
+      else if(birthDay == 4){
         alert("You were born on "+dayNames[3] +  " and Your akan name is " +maleNames[3]+"!");
       }
-      else if(dayValue == 5){
+      else if(birthDay == 5){
         alert("You were born on "+dayNames[4] +  " and Your akan name is " +maleNames[4]+"!");
       }
-      else if(dayValue == 6){
+      else if(birthDay == 6){
         alert("You were born on "+dayNames[5] +  " and Your akan name is " +maleNames[5]+"!");
       }
-      else if(dayValue == -0){
+      else if(birthDay == -0){
         alert("You were born on "+dayNames[6] + " and Your akan name is " +maleNames[6]+"!");
       }
     break;
     case "female":
-      if (dayValue == 1){
+      if (birthDay == 1){
         alert("You were born on "+dayNames[0] + " and Your akan name is  " +femaleNames[0]+"!");
       }
-      else if(dayValue == 2){
+      else if(birthDay == 2){
         alert("You were born on " +dayNames[1] + " and Your akan name is " +femaleNames[1]+"!");
       }
-      else if(dayValue == 3){
+      else if(birthDay == 3){
         alert("You were born on " +dayNames[2] + " and Your akan name is " +femaleNames[2]+"!");
       }
-      else if(dayValue == 4){
+      else if(birthDay == 4){
         alert("You were born on " +dayNames[3] + " and Your akan name is " +femaleNames[3]+"!");
       }
-      else if(dayValue == 5){
+      else if(birthDay == 5){
         alert("You were born on " +dayNames[4] + " and Your akan name is " +femaleNames[4]+"!");
       }
-      else if(dayValue == 6){
+      else if(birthDay == 6){
         alert("You were born on " +dayNames[5] + " and Your akan name is " + femaleNames[5]+"!");
-      }else if(dayValue == -0){
+      }else if(birthDay == -0){
         alert("You were born on " +dayNames[6] + " and Your akan name is " +femaleNames[6]+"!");
       }
     break
@@ -105,8 +105,7 @@ function getGender(){
   }
 }
 function findName(){
-  dayValue = calculateDayValue();
+  birthDay = calculatebirthDay();
   getGender();
 
-  alert(dayValue);
 }
